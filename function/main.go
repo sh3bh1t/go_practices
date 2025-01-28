8b83605ee83e537bb9e2e29e55068445ff10a6b8
sh3bh1t <shobhits2000@gmail.com>
2025-01-28 14:31:23 +0530

    go functions

--- function/main.go
+++ function/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+func simpFunction() {
+	fmt.Println("wohooooooo")
+}
+
+func add(a, b int) int { //here a and b are integer paramaters and int is the return type
+	return a + b
+}
+
 func main() {
 	fmt.Println("today learning about functions in go")
+	simpFunction()
+	fmt.Println(add(69, 69))
 }
