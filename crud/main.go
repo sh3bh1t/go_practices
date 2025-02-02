09b9bf9b3cc5efd08162f073bcd1bb353b05cbbc
sh3bh1t <shobhits2000@gmail.com>
2025-02-02 13:57:02 +0530

    crud completed

--- crud/main.go
+++ crud/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 // struct to store the data from response of GET method
@@ -14,9 +16,7 @@ type Todo struct {
 	Completed bool   `json="status"`
 }
 
-func main() {
-	fmt.Println("learning CRUD IN GO...... ")
-
+func performGetRequest() {
 	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
 	if err != nil {
 		fmt.Println("the error encounterd is : ", err)
@@ -49,4 +49,118 @@ func main() {
 	}
 	fmt.Println("TODO : ", todo)
 
+}
+
+func performPostRequest() {
+
+	todo := Todo{
+		UserId:    24,
+		Title:     "tmkc",
+		Completed: true,
+	}
+
+	myUrl := "https://jsonplaceholder.typicode.com/todos"
+
+	//convert todo data into JSON
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("error marshaling : ", err)
+		return
+	}
+
+	// convert the json data to string
+	jsonString := string(jsonData)
+
+	//convert string data to reader
+	jsonReader := strings.NewReader(jsonString)
+
+	// sending POST request
+	res, err := http.Post(myUrl, "application/json", jsonReader)
+	if err != nil {
+		fmt.Println("error sending req : ", err)
+		return
+	}
+	defer res.Body.Close()
+
+	data, _ := ioutil.ReadAll(res.Body)
+	fmt.Println("response: ", string(data))
+	fmt.Println("response status: ", res.Status)
+
+}
+
+func performUpdateRequest() {
+	todo := Todo{
+		UserId:    2989,
+		Title:     "tmkcOnceAgain",
+		Completed: false,
+	}
+
+	myUrl := "https://jsonplaceholder.typicode.com/todos/1"
+
+	//convert todo data into JSON
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("error marshaling : ", err)
+		return
+	}
+
+	//create PUT request
+	//GO library of http only has GET and POST methods so for PUT method we need to create a new request and that function takes three parameters in which the third one is a reader form
+
+	// convert the json data to string
+	jsonString := string(jsonData)
+
+	//convert string data to reader
+	jsonReader := strings.NewReader(jsonString)
+
+	//create PUT req
+	req, err := http.NewRequest(http.MethodPut, myUrl, jsonReader)
+	if err != nil {
+		fmt.Println("error putting : ", err)
+		return
+	}
+	req.Header.Set("Content-type", "application/json")
+
+	//send the request
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("error sending request : ", err)
+		return
+	}
+	defer res.Body.Close()
+	data, _ := ioutil.ReadAll(res.Body)
+	fmt.Println("response: ", string(data))
+	fmt.Println("response status  for putting : ", res.Status)
+}
+
+func performDeleteRequest() {
+	myUrl := "https://jsonplaceholder.typicode.com/todos/1"
+
+	//create DELETE req
+	req, err := http.NewRequest(http.MethodDelete, myUrl, nil)
+	if err != nil {
+		fmt.Println("error deleting : ", err)
+		return
+	}
+	//send the request
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("error sending request : ", err)
+		return
+	}
+	defer res.Body.Close()
+	data, _ := ioutil.ReadAll(res.Body)
+	fmt.Println("response: ", string(data))
+	fmt.Println("response status  for putting : ", res.Status)
+}
+
+func main() {
+	fmt.Println("learning CRUD IN GO...... ")
+
+	//performGetRequest()
+	//performPostRequest()
+	//performUpdateRequest()
+	performDeleteRequest()
 }
